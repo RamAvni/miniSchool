d64976af15dd791f8c8823dc89f993146765215d
RamAvni <ramavni@hilma.tech>
2024-11-26 16:29:57 +0200

    teacher patch

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const { con } = require("../con");
 
 router.patch("/", (req, res) => {
-    res.send("Hi");
+    con.query(
+        `UPDATE teacher SET name = '${req.body.name}', school_code = ${req.body["school_code"]}, password = ${req.body.password}, email = '${req.body.email}' WHERE id = ${req.body.id}`,
+        (err) => {
+            if (err) throw err;
+        }
+    );
+
+    res.send("success");
 });
 
 module.exports = router;
